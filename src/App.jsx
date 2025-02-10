6d0d5bea64c34623297f84b5c94e2d76948b3e8c
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-02-10 17:55:06 +0530

    Added List and Item components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Form from './components/Form'
+import Empty from './components/Empty'
+import List from './components/List'
 
 function App() {
   return (
     <>
       <Header />
-
+      
       <div className="container my-5">
         <div className="row">
           <div className="col">
-            <h1>Hello World!!!</h1>
+            <Form />
+            <List />
+            <Empty />
           </div>
         </div>
       </div>
-      
+
       <Footer />
     </>
   )
