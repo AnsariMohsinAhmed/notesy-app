f6afd38412b6f030e577adfd441a38f099c85bcd
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-02-10 11:49:45 +0530

    added header, footer and booswatch theme.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <>
-      <h1>Hello World!!!</h1>
+      <Header />
+
+      <div className="container my-5">
+        <div className="row">
+          <div className="col">
+            <h1>Hello World!!!</h1>
+          </div>
+        </div>
+      </div>
+      
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <>
+            <div>All rights reserved &copy; {(new Date().getFullYear())}</div>
+        </>
+    )
+}
+
+export default Footer;
